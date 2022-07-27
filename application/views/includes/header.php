9879e550d418e117f44e6facd3af0eed7ffb46c3
hamdi Yıldız <hamdiyildiz06@gmail.com>
2022-07-27 22:44:24 +0300

    galeri kısmı komple kaldırıldı

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -112,15 +112,6 @@
                                             <a href="<?php echo base_url("markalar"); ?>" class="nav-link">Markalar</a>
                                         </li>
 
-                                        <li class="nav-item dropdown ">
-                                            <a href="#" class="nav-link dropdown-toggle" id="third-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Galeriler</a>
-                                            <ul class="dropdown-menu" aria-labelledby="third-dropdown">
-                                                <li ><a href="<?php echo base_url("fotograf-galerisi"); ?>">Resim Galerisi</a></li>
-                                                <li ><a href="<?php echo base_url("video-galerisi"); ?>">Video Galerisi</a></li>
-                                                <li ><a href="<?php echo base_url("dosya-galerisi"); ?>">Dosya Galerisi</a></li>
-                                            </ul>
-                                        </li>
-
                                     </ul>
                                     <!-- main-menu end -->
                                 </div>
