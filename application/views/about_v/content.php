2e0bb8b789f2e3a6b5b6b68c400953f5d099e62e
hamdi Yıldız <hamdiyildiz06@gmail.com>
2022-07-29 15:05:33 +0300

    about_us kısmında arka plan resmi atadım

--- application/views/about_v/content.php
+++ application/views/about_v/content.php
@@ -12,12 +12,12 @@
                 <div class="separator-2"></div>
                 <div class="row mb-5 mt-5">
 
-                    <div class="col-md-6">
+                    <div class="col-md-12 responsive"  style="background-image:url('<?php echo base_url("assets/images/roofsoft.jpg");?>'); background-position: 50% 21%; background-repeat: no-repeat;">
                         <?php echo $settings->about_us; ?>
                     </div>
-                    <div class="col-md-6">
-                        <img class="responsive" src="<?= base_url("assets/images/roof.jpg"); ?>" alt="roofsocialstudio">
-                    </div>
+<!--                    <div class="col-md-6">
+                        <img class="responsive" src="<?/*= base_url("assets/images/roof.jpg"); */?>" alt="roofsocialstudio">
+                    </div>-->
 
                 </div>
             </div>
